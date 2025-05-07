c124ca0859b1d8eddf298d3d3f6e103c02eb7c43
fourkimm <fourkimm@iabacus.co.kr>
2025-05-07 18:53:23 +1000

    feat: mongoDb install

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://fourkimm:dudgusdldi96@joy.pnd427d.mongodb.net/?retryWrites=true&w=majority&appName=Joy"
+  )
+  .then(() => console.log("MongoDb Connected..."))
+  .catch((err) => console.log(">>>>>>", err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
